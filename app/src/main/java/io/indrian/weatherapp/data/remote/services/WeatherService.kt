766d337a2e20477cec549483cbcba03cb7e1e7a8
INDRIAN <indrian16@outlook.co.id>
2021-02-10 17:37:06 +0800

    add unit testing for weather service

--- app/src/main/java/io/indrian/weatherapp/data/remote/services/WeatherService.kt
+++ app/src/main/java/io/indrian/weatherapp/data/remote/services/WeatherService.kt
@@ -1,22 +1,23 @@
 package io.indrian.weatherapp.data.remote.services
 
 import io.indrian.weatherapp.data.remote.responses.WeatherResponse
+import io.indrian.weatherapp.utils.AppConstants
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WeatherService {
 
-    @GET("/weather")
+    @GET("data/2.5/weather")
     fun getWeatherByCity(
         @Query("q") city: String,
-        @Query("appid") appId: String
+        @Query("appid") appId: String = AppConstants.APP_ID
     ): Single<WeatherResponse>
 
-    @GET("/weather")
+    @GET("data/2.5/weather")
     fun getWeatherCoordinate(
         @Query("lat") lat: String,
         @Query("lon") lon: String,
-        @Query("appid") appId: String
+        @Query("appid") appId: String = AppConstants.APP_ID
     ): Single<WeatherResponse>
 }
--- app/src/main/java/io/indrian/weatherapp/di/NetworkModule.kt
+++ app/src/main/java/io/indrian/weatherapp/di/NetworkModule.kt
@@ -1,8 +1,36 @@
 package io.indrian.weatherapp.di
 
+import io.indrian.weatherapp.data.remote.services.WeatherService
+import io.indrian.weatherapp.utils.AppConstants
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 val networkModule = module {
 
-    //factory<Interceptor> {  }
+    factory<Interceptor> {
+        HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
+    factory {
+        OkHttpClient.Builder()
+            .addInterceptor(get<Interceptor>())
+            .build()
+    }
+
+    single {
+        Retrofit.Builder()
+            .baseUrl(AppConstants.BASE_URL)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    factory { get<Retrofit>().create(WeatherService::class.java) }
 }
