cbcc89b32bf84b1266eade7f81688fb8b9a1e189
INDRIAN <indrian16@outlook.co.id>
2021-02-10 16:42:21 +0800

    define API DATA to Gradle

---
+++ app/src/main/java/io/indrian/weatherapp/utils/AppConstants.kt
@@ -0,0 +1,9 @@
+package io.indrian.weatherapp.utils
+
+import io.indrian.weatherapp.BuildConfig
+
+object AppConstants {
+
+    const val BASE_URL = BuildConfig.BASE_URL
+    const val APP_ID = BuildConfig.APP_ID
+}
