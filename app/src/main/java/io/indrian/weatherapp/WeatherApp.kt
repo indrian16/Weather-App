25ba32c6ea12680a2db4061f12d4c2a257fbd216
INDRIAN <indrian16@outlook.co.id>
2021-02-10 19:46:22 +0800

    inject data module and enable logger mode debug

--- app/src/main/java/io/indrian/weatherapp/WeatherApp.kt
+++ app/src/main/java/io/indrian/weatherapp/WeatherApp.kt
@@ -1,5 +1,29 @@
 package io.indrian.weatherapp
 
 import android.app.Application
+import io.indrian.weatherapp.di.localModule
+import io.indrian.weatherapp.di.remoteModule
+import io.indrian.weatherapp.di.repositoryModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import timber.log.Timber
 
-class WeatherApp : Application()
+class WeatherApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+        startKoin {
+            androidLogger()
+            androidContext(this@WeatherApp)
+            modules(
+                remoteModule,
+                localModule,
+                repositoryModule
+            )
+        }
+    }
+}
--- app/src/main/java/io/indrian/weatherapp/di/RemoteModule.kt
+++ app/src/main/java/io/indrian/weatherapp/di/RemoteModule.kt
@@ -10,7 +10,7 @@ import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
-val networkModule = module {
+val remoteModule = module {
 
     factory<Interceptor> {
         HttpLoggingInterceptor().apply {
